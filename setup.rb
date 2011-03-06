a6b040e499f85cef8009a7ba5be2b46d069709ba
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-03-06 18:39:35 -0500

    setup.rb doing all renames now

--- setup.rb
+++ setup.rb
@@ -1,11 +1,15 @@
 #!/usr/bin/env ruby
 
+def to_underscore(string)
+  string.gsub(/(.)([A-Z])/,'\1_\2').downcase!
+end
+
 if ARGV.first.nil?
   puts 'Usage: pass a CamelCased name for your app, and this program will replace occurences of GenericApp and generic_app with your name.'
   exit 1
 else
   appName = ARGV.first # CamelCase version
-  # app_name = # under_score version
+  app_name = appName.gsub(/(.)([A-Z])/,'\1_\2').downcase # under_score version
 end
 
 FILES = [
@@ -23,5 +27,8 @@ FILES = [
 
 FILES.each do |filename|
   text = File.read(filename).gsub('GenericApp', appName)
+  File.open(filename, 'w') { |file| file.puts text }
+
+  text = File.read(filename).gsub('generic_app', app_name)
   File.open(filename, 'w') { |file| file.puts text }
 end
