f6375976dfee21316e544f7a372e86578fe342be
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-04-27 21:55:22 -0400

    proper sort order

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,13 @@
 class TasksController < ApplicationController
+  before_filter :find_user, :only => [:index]
+
+  def index
+    @tasks = @user.tasks.order('rank DESC')
+  end
+
+  private
+
+  def find_user
+    @user = current_user
+  end
 end
