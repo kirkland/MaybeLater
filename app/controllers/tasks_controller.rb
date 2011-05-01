2f36f669d775502e615a7b8472dda6f2663423e5
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-05-01 19:03:55 -0400

    add ability to create a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,14 @@
 class TasksController < ApplicationController
-  before_filter :find_user, :only => [:index, :reorder]
+  before_filter :find_user, :only => [:index, :reorder, :create]
 
   def index
+    @task = Task.new
     @tasks = @user.ordered_tasks
   end
 
   def create
-    create! { new_task_path }
+    t = Task.create!(params[:task].merge(:user => @user))
+    render :json => t
   end
 
   def update
@@ -17,8 +19,6 @@ class TasksController < ApplicationController
     @user.set_ordered_task_ids(params[:ordered_task_ids].map(&:to_i))
     render :text => @user.ordered_task_ids.inspect
   end
-
-  private
 
   def find_user
     @user = current_user
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,9 @@
 class Task < ActiveRecord::Base
   belongs_to :user
+
+  after_create :update_user_ordered_task_ids
+
+  def update_user_ordered_task_ids
+    user.update_attribute(:ordered_task_ids, user.ordered_task_ids.unshift(id))
+  end
 end
