1dd873651e88fa67e1d4517958e971c0938c70ce
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-05-01 19:19:08 -0400

    ajaxify create form. emacs js mode is retarded

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,22 @@
+function setupTasksIndex(updatePath, createPath) {
+  setupTasksDragDrop(updatePath);
+  setupNewTaskForm(createPath);
+}
+
+function setupNewTaskForm(createPath) {
+  $('#task_submit').click(event, function () {
+	  event.preventDefault();
+	  //    console.log($('#new_task').serialize());
+        $.ajax(createPath, {
+    	    type: 'POST',
+	    data: $('#new_task').serialize()
+		    //		    success: function(data) { console.log(data);},
+		    //            error: function(data) { console.log("error"); console.log(data); }
+		    })
+	      
+      });
+}
+
 var fixHelper = function(e, ui) {
   ui.children().each(function() {
       $(this).width($(this).width());
