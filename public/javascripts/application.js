dfccace728ce433689584c3f4763b5cefebea9d6
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-05-19 13:56:18 -0400

    convert just about all to coffeescript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,67 +1,83 @@
-function setupTasksIndex(updatePath, createPath) {
-  setupNewTaskForm(createPath);
-}
+/* DO NOT MODIFY. This file was compiled Thu, 19 May 2011 17:55:34 GMT from
+ * /home/rob/code/maybe_later/app/coffeescripts/application.coffee
+ */
 
-function insertNewTask(title, id) {
-  var taskData = {
-    title: title,
-    'id': id
-  };
-
-  var newRow = $('.task_template').clone().directives({'.task_title': 'title', '@data-id': 'id'}).render(taskData).removeClass('task_template');
-  $('#tasks tbody').prepend(newRow);
-
-  return newRow;
-}
-
-function setupNewRow(newRow, id, updatePath) {
-  newRow.attr('data-id', id);
-  newRow.attr('data-update_path', updatePath);
-  newRow.find('td').removeClass('just_added');
-
-  newRow.find('.actions .complete_link').click(event, function() {
-    event.preventDefault();
-//    updateStatus(Ssomeargshere);
-  });            
-}
-
-function setupNewTaskForm(createPath) {
-  $('#task_submit').click(event, function () {
-    event.preventDefault();
-    var ajaxData = $('#new_task').serialize();
-    var title = $('#task_title').val();
-    $('#task_title').val('');
-
-    // add new row immediately. we'll populate data attributes when AJAX returns
-    var newRow = insertNewTask(title);
-    
-    $.ajax(createPath, {
-      type: 'POST',
-      data: ajaxData,
-      success: function(data) {
-        setupNewRow(newRow, data.task.id, data.task.update_path);
-      },
-      error: function(data) {
-      }
-    });
-  });
-}
-
-
-function updateStatus(ajax_path, task_id, defer_time) {
-  $(event.target).closest('tr').remove();
-  
-  $.ajax(ajax_path, {
-    type: 'POST',
-    data: {
-      task_id: task_id,
-      defer_time: defer_time
-    },
-    success: function(data) {
-//      console.log(data);
-    },
-    error: function(data) {
-//      console.log(data.responseText);
+(function() {
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  this.TasksIndex = (function() {
+    function TasksIndex(reorderPath, createPath) {
+      this.reorderPath = reorderPath;
+      this.createPath = createPath;
+      this.setupTasksDragDrop();
+      this.setupNewTaskForm();
+      $('#task_title').focus();
     }
-  });
-}
+    TasksIndex.prototype.setupTasksDragDrop = function() {
+      var fixHelper;
+      fixHelper = function(e, ui) {
+        ui.children().each(function() {
+          return $(this).width($(this).width());
+        });
+        return ui;
+      };
+      return $('#tasks tbody').sortable({
+        items: 'tr',
+        helper: fixHelper,
+        containment: "#tasks tbody",
+        tolerance: 'pointer',
+        update: __bind(function(event, ui) {
+          var newTasksOrder;
+          newTasksOrder = $.map($('#tasks tr').not('.task_template'), function(t) {
+            return $(t).attr('data-id');
+          });
+          return $.ajax(this.reorderPath, {
+            type: 'POST',
+            data: {
+              ordered_task_ids: newTasksOrder
+            }
+          });
+        }, this)
+      }).disableSelection();
+    };
+    TasksIndex.prototype.insertNewTask = function(title, id) {
+      var newRow, taskData;
+      taskData = {
+        title: title,
+        'id': id
+      };
+      newRow = $('.task_template').clone().directives({
+        '.task_title': 'title',
+        '@data-id': 'id'
+      }).render(taskData).removeClass('task_template');
+      $('#tasks tbody').prepend(newRow);
+      return newRow;
+    };
+    TasksIndex.prototype.setupNewRow = function(newRow) {
+      return newRow.find('.actions .complete_link').click(event, function() {
+        return event.preventDefault();
+      });
+    };
+    TasksIndex.prototype.setupNewTaskForm = function() {
+      return $('#task_submit').click(event, __bind(function() {
+        var ajaxData, newRow, title;
+        event.preventDefault();
+        ajaxData = $('#new_task').serialize();
+        title = $('#task_title').val();
+        $('#task_title').val('');
+        newRow = this.insertNewTask(title);
+        return $.ajax(this.createPath, {
+          type: 'POST',
+          data: ajaxData,
+          success: __bind(function(data) {
+            newRow.attr('data-id', data.task.id);
+            newRow.attr('data-update_path', data.task.updatePath);
+            newRow.find('td').removeClass('just_added');
+            return this.setupNewRow(newRow);
+          }, this),
+          error: function(data) {}
+        });
+      }, this));
+    };
+    return TasksIndex;
+  })();
+}).call(this);
---
+++ public/javascripts/old_application.js
@@ -0,0 +1,18 @@
+
+// function updateStatus(ajax_path, task_id, defer_time) {
+//   $(event.target).closest('tr').remove();
+  
+//   $.ajax(ajax_path, {
+//     type: 'POST',
+//     data: {
+//       task_id: task_id,
+//       defer_time: defer_time
+//     },
+//     success: function(data) {
+// //      console.log(data);
+//     },
+//     error: function(data) {
+// //      console.log(data.responseText);
+//     }
+//   });
+// }
