749a8332d1e04212a4d6ba21467bf18ba020d14d
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-08-25 13:17:48 -0400

    maybe fix first request issue

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,3 +1,7 @@
 require 'compass'
 require 'compass/app_integration/rails'
+FileUtils.mkdir_p(Rails.root.join('tmp', 'stylesheets'))
+Rails.configuration.middleware.insert_before('Rack::Sendfile', 'Rack::Static',
+    :urls => ['/stylesheets'],
+    :root => "#{Rails.root}/tmp")
 Compass::AppIntegration::Rails.initialize!
